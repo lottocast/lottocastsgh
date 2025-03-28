f28c6b2a118db2f872fe57aa7a2640aad980a1ef
ceolottocasts <ceo@lottocasts.com>
2025-03-28 18:02:47 +0900

    fix: apply custom Vite config with correct HTML input

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  // public/index.html을 entry point로 인식하도록 설정
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
+      input: 'public/index.html',
     },
+    outDir: 'dist',
   },
+  publicDir: false, // public 디렉토리는 static 처리 안 하도록 막기
+  plugins: [react()],
 })
