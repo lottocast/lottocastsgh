9f0553876e39395e24ba496b0583ef98482da7fd
ceolottocasts <ceo@lottocasts.com>
2025-03-28 18:07:17 +0900

    fix: move index.html to root and clean vite config

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // public/index.html을 entry point로 인식하도록 설정
-  build: {
-    rollupOptions: {
-      input: 'public/index.html',
-    },
-    outDir: 'dist',
-  },
-  publicDir: false, // public 디렉토리는 static 처리 안 하도록 막기
   plugins: [react()],
 })
